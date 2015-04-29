edf550d7878802f3c68c9279a149ed8c76ab75ba
summanr <summanrafia@gmail.com>
2015-04-28 21:10:27 -0700

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -1,11 +1,17 @@
 
 
-class Analysis:
+import itertools
+import re
+from parseDataToClass import *
+from main import *
+from collections import Counter
 
+
+class Analysis:
     translatedFrame1 = {}
     translatedFrame2 = {}
     newFrame = ''
-
+    b = []
 
     def __init__(self):
         pass
@@ -13,7 +19,7 @@ class Analysis:
 
     def discount(self, alpha, mass):
         try:
-            #print("Entered discount operation")
+            # print("Entered discount operation")
             alpha = float(alpha)
             mass = float(mass)
 
@@ -24,12 +30,12 @@ class Analysis:
                 self.discounted = float(alpha) * float(mass)
                 return self.discounted
 
-        except (TypeError,ValueError):
+        except (TypeError, ValueError):
             print("Must be a numeric value and not a string value.\n")
             print("Please recheck the input text file and make sure all values are correct.")
 
 
-    def translate(self,frame1, relations1, frame2, relations2):
+    def translate(self, frame1, relations1, frame2, relations2):
         print("Entered Translating Operation\n")
 
         x = 3
@@ -50,12 +56,12 @@ class Analysis:
         self.translatedFrame1.clear()
         self.translatedFrame2.clear()
 
-        #iterate through an unknown length of the Frame 1, and collect all the belief values
-        #record the translated frame in a dictionary
+        # iterate through an unknown length of the Frame 1, and collect all the belief values
+        # record the translated frame in a dictionary
         while x < length_ofFrame1:
             try:
                 for j in relations2:
-                    proposition = frame1[x+1]
+                    proposition = frame1[x + 1]
                     key = proposition + ' v ' + str(j)
                     value = float(frame1[x])
                     mass = float(mass) + value
@@ -93,11 +99,109 @@ class Analysis:
         return self.translatedFrame1, self.translatedFrame2, self.newFrame
 
 
-    #Dempster's combination rule
-    def fuse(self,translatedFrame1, translatedFrame2):
+    # Dempster's combination rule
+    def fuse(self, massA, massB):
+        i = 0
+        a = 0
+        sum1 = []
+        sumk = []
+        for key, value in massB.items():
+            for keys, values in massA.items():
+                print(((keys)), ((key)))
+                if keys == 'theta' or key == 'theta':
+                    print(values)
+                    print(value)
+                    result = (value * values)
+                    print(result)
+                    sum1.append(result)
+                    if key == "theta" and keys == "theta":
+                        x = ("theta"), (result)
+                        self.b.append(x)
+                    elif key == "theta" and keys != "theta":
+                        x = keys, result
+                        self.b.append(x)
+                    elif key != "theta" and keys == "theta":
+                        x = key, result
+                        self.b.append(x)
+                elif keys != "theta" and key != "theta":
+                    count = 0
+                    count1 = 0
+                    print ("here")
+                    s1 = set(key.split(' '))
+                    s2 = set(keys.split(' '))
+                    if any(s1.intersection(s2)):
+                        print("found a match!")
+                        print(values)
+                        print(value)
+                        result1 = (value * values)
+                        print(result1)
+                        sum1.append(result1)
+                        x = (key, result1)
+                        self.b.append(x)
+                        break
+                    else:
+                        print("No Match")
+                        print(value)
+                        print(values)
+                        result2 = (1 - (value * values))
+                        print(result2)
+                        sumk.append(result2)
+                        x = (key, result2)
+                        self.b.append(x)
 
-        pass
+            i = i + 1
+            a = a + 1
+        print(self.b)
+        print(sum1)
+        sum1 = sum(sum1)
+        print(sum1)
+        print(sumk)
+        sumk = sum(sumk)
+        print(sumk)
+        if sumk != 0:
+            K = 1 / sumk
+            print("K = ", K)
+            m1x2 = K * sum1
+            print("The mass of 1 and 2 is : ", (m1x2))
+        else:
+            m1x2 = sum1
+            print("The mass of 1 and 2 is : ", (m1x2))
 
 
-    def interpret(self):
-        pass
+
+    def interpret(self, b, newDirectory ):
+        output_dir =open(newDirectory + "Output.txt", 'w')
+        print (b)
+        support = 0
+        val = []
+        for keys,values in b:
+            print (keys)
+            print (values)
+            val.append(values)
+            print (val)
+        val = sum(val)
+        print (val)
+        for keys, values in b:
+            counter = (Counter(keys) in b)
+            print (keys)
+            print (values)
+            if counter == True:
+                print ('sum all')
+                support = sum(values)
+                print (("Support : "), support)
+                plausibility = abs((1-((val)-values)))
+                print (("Plausibility: "),plausibility)
+                EI = [support, plausibility]
+                print ("EI:", EI)
+            else:
+                support = values
+                print (("Support : "), support)
+                plausibility = abs((1-((val)-values)))
+                print (("Plausibility: "),plausibility)
+                EI = [support, plausibility]
+                print ("EI:", EI)
+
+
+
+
+

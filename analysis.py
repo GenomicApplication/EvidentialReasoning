d7d2aec6eb6d82cb002ff1c9daaa5fd4c5bd435d
GenomicApplication <tami.thi.le@gmail.com>
2015-05-04 22:10:22 -0700

    It is not ready for you to pass in whatever you need to write without having to reorganize

--- analysis.py
+++ analysis.py
@@ -254,7 +254,7 @@ class Analysis:
 
         file_write('\n\n')
 
-        #self.fuse(self.translatedFrame1, self.translatedFrame2)
+        self.fuse(self.translatedFrame1, self.translatedFrame2)
 
         return self.translatedFrame1, self.translatedFrame2, self.newFrame
 
